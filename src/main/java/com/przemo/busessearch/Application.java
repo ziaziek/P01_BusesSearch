815c26f87e48f04fb46d31f382404f5caf970adf
Przemo <Przemo@192.168.2.4>
2015-04-18 18:37:49 +0100

    Spring integrated into the application

--- src/main/java/com/przemo/busessearch/Application.java
+++ src/main/java/com/przemo/busessearch/Application.java
@@ -7,6 +7,7 @@ package com.przemo.busessearch;
 
 import org.apache.wicket.Page;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
 
 /**
  *
@@ -18,6 +19,11 @@ public class Application extends WebApplication {
     public Class<? extends Page> getHomePage() {
         return HomePage.class;
     }
-    
+
+    @Override
+    protected void init() {
+        super.init();
+        getComponentInstantiationListeners().add(new SpringComponentInjector(this));
+    }
     
 }
